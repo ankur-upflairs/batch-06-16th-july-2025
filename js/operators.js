fa94e94d27bd83829eaa0424567247f12f41b153
ankur-upflairs <ankur.upflairs@gmail.com>
2025-07-29 15:57:07 +0530

    code pushed

---
+++ js/operators.js
@@ -0,0 +1,41 @@
+// arithmatic operators 
+console.log(23%5)
+// Arithmetic operators are used to perform mathematical calculations.
+let x = 10, y = 3;
+console.log(x + y);   // Addition: 13
+console.log(x - y);   // Subtraction: 7
+console.log(x * y);   // Multiplication: 30
+console.log(x / y);   // Division: 3.3333...
+console.log(x % y);   // Modulus (remainder): 1
+console.log(x ** y);  // Exponentiation (x to the power of y): 1000
+
+//assignment operators
+let b =5;
+b+=4;//a=a+4 
+// Assignment operators assign values to JavaScript variables.
+
+let a = 10;
+a += 5;   // a = a + 5: 15
+a -= 2;   // a = a - 2: 13
+a *= 2;   // a = a * 2: 26
+a /= 4;   // a = a / 4: 6.5
+a %= 3;   // a = a % 3: 0.5
+a **= 2;  // a = a ** 2: 0.25
+console.log(a)
+//comparison operators
+// Comparison operators are used to compare two values.
+let c = 5, d = 10;
+console.log(c == d);    // Equal: false
+console.log(c === d);   // Strict equal: false
+console.log(c != d);    // Not equal: true
+console.log(c !== d);   // Strict not equal: true
+console.log(c < d);     // Less than: true
+console.log(c > d);     // Greater than: false
+console.log(c <= d);    // Less than or equal to: true
+console.log(c >= d);    // Greater than or equal to: false
+
+
+//logical operators
+
+
+
