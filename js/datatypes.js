a434f2843b4e6fdde3d52787c9a821ab9583787b
ankur-upflairs <ankur.upflairs@gmail.com>
2025-07-29 15:25:25 +0530

    code pushed

---
+++ js/datatypes.js
@@ -0,0 +1,42 @@
+let a = 45
+// console.log(a)
+//string 
+let b = 'hello'
+let c ="everyone"
+// console.log(b,c)
+console.log(typeof b)
+
+//boolean 
+let d =true 
+console.log(d,typeof d)
+//undefined
+let e =undefined
+let f ;
+console.log(e,typeof f)
+//NUll
+let g = null
+console.log(g,typeof null)
+//bigInt 
+let h =34545656n
+console.log(h,typeof h)
+//symbol
+let s1 = Symbol('a')
+let s2 = Symbol('a')
+// console.log(s1==s2)
+//object => key -value pair
+let person = {
+    name:'pawan',
+    age: 23,    
+}
+// console.log(person.name)
+// console.log(person['age'])
+let marks = [45,23,78]
+
+// console.log(marks[1])
+
+
+
+
+
+
+
