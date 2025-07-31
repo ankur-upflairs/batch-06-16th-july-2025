a062f2382d7031c32f9bad96a718f197d4c98b94
ankur-upflairs <ankur.upflairs@gmail.com>
2025-07-31 14:59:06 +0530

    code pushed

---
+++ js/loop.js
@@ -0,0 +1,46 @@
+// //starting of loop 
+// let i =1 ;
+// //end condition 
+// while(i < 11){
+//     //code 
+//     console.log(i,'hello')
+//     //update condition
+//     i++;
+// }
+// for(let i =10;i>0 ; i--){
+//     console.log(i)
+// }
+
+//do-while => 
+//print 1-100 ,skipping 5 (eg- 1,6,11,16)
+// let i =1;
+// do{
+//     //code
+//     console.log(i)
+//     i=i+5;
+// }while(i<=100)
+
+/*
+Print numbers from 1 to 10 using a for loop.
+
+Print numbers from 10 to 1 using a while loop.
+
+Print even numbers between 1 and 20 using a loop.
+
+Print odd numbers from 1 to 30 using a for loop.
+
+Print all numbers divisible by 5 from 1 to 50.
+
+➕ Sum, Product, and Math Logic
+Find the sum of numbers from 1 to 100.
+
+Find the product (multiplication) of numbers from 1 to 10.
+
+Print the multiplication table of 7 (from 7×1 to 7×10).
+
+Count how many numbers between 1 and 50 are divisible by 3.
+
+Find the factorial of a number using a loop (e.g., 5! = 120).
+*/
+
+
