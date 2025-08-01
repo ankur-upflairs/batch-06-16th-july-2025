7436359a48e6177a69bdb70363d0338c46856046
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-01 16:15:10 +0530

    code pushed

---
+++ js/array.js
@@ -0,0 +1,18 @@
+//array => collecction of data types 
+let a = [34,true,'hello']
+// console.log(a[0]) 
+// console.log(a.at(1)) 
+// console.log(a.length)
+let arr = [34,45,23,12,9,4] 
+// arr.pop()//removes last element
+// arr.shift()
+// arr.push(22)//add element at last
+// arr.unshift(11)
+// console.log(arr)
+// arr.splice(1,0,33)
+// console.log(arr)
+// console.log(arr.slice(1,3))
+// console.log(arr.concat([1,1]))
+// console.log(arr.fill(0,1,4))
+// console.log(arr)
+
