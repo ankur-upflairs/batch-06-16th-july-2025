fffaaaaf546975691c8ec52e8d1030e1cb20627c
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-04 15:58:11 +0530

    code pushed

--- js/dom/dom1.js
+++ js/dom/dom1.js
@@ -9,11 +9,36 @@
 // let e = document.querySelectorAll('.x') //node list 
 // console.log(e)
 //accessing text and html =>
-let f = document.getElementById('a')
-console.log(f.innerText)
-console.log(f.innerHTML)
+// let f = document.getElementById('a')
+// console.log(f.innerText)
+// console.log(f.innerHTML)
 // f.innerText = 'changed text'
 // f.innerHTML = '<b>hello everyone</b>'
+function show(){
+    alert('button clicked') 
+}
+// let button = document.getElementById('btn')
+// button.addEventListener('mouseenter',function(){
+//     // alert('mouse entered on button')
+//     button.innerText = 'mouse is on'
+// })
+// button.addEventListener('mouseleave',function () {
+//     button.innerText = 'mouse is out'
+// })
+//changing styles 
+let a = document.getElementById('a')
+let btn1 = document.getElementById('btn1')
+btn1.addEventListener('click',function(){
+    //remove hyphen and convert it to camelcase 
+    //and value must be string
+    a.style.color = 'red'
+    a.style.backgroundColor = 'blue'
+})
+//make a button that show a box onclick and a button that hides the same box
+//make button to change the theme of webpage(dark/light)
+
+
+
 
 
 
