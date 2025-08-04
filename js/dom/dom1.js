5f7105c637387d8d0a03eea0206a2f740ab3843e
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-04 15:10:16 +0530

    code pushed

---
+++ js/dom/dom1.js
@@ -0,0 +1,21 @@
+// let a = document.getElementsByTagName('p') //html collection
+// // console.log(a)
+// let b = document.getElementsByClassName('x')//html collection
+// console.log(b)
+// let c = document.getElementById('y') //element 
+// // console.log(c) 
+// let d = document.querySelector('.x') //element
+// // console.log(d) 
+// let e = document.querySelectorAll('.x') //node list 
+// console.log(e)
+//accessing text and html =>
+let f = document.getElementById('a')
+console.log(f.innerText)
+console.log(f.innerHTML)
+// f.innerText = 'changed text'
+// f.innerHTML = '<b>hello everyone</b>'
+
+
+
+
+
