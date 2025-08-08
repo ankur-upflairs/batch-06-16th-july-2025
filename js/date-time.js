9b61ad6358ee4a95bc2316ab9d50167cecd83948
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-08 15:39:15 +0530

    code pushed

---
+++ js/date-time.js
@@ -0,0 +1,12 @@
+let date = new Date()
+// console.log(date)
+//year
+// console.log(date.getFullYear())
+// date.setFullYear(2000)
+// console.log(date)
+// console.log(date.getMonth())
+//0-jan , 11 -dec
+// date.setMonth(1)
+// console.log(date)
+let dob = new Date(2000,11,1)
+console.log(dob)
