14b14091bfb7679618a7237ee6956f3f9d7f3102
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-12 15:25:11 +0530

    code pushed

--- react/props/src/Content.jsx
+++ react/props/src/Content.jsx
@@ -4,17 +4,24 @@
 //remove hyphen (-) and convert it to the camel case 
 //every value must be string 
 
+import { Fragment } from "react"
+
 function Content(){
   return <div style={{ color:'red',backgroundColor:'blue',border:'1px solid'}} >
     this is Content component
     </div>
 
 }
-
+//fragments are replacement of parent div in jsx 
 function Footer(){
-  return <div>
+  return <>
     this is Footer component
-    </div>
+    </>  
 }
+// function Footer(){
+//   return <Fragment>
+//     this is Footer component
+//     </Fragment>  
+// }
 
 export {Content,Footer}
