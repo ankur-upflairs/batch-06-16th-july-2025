efbaaac6c82e8049d511cac69f5cba6b086e96f2
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-20 15:58:51 +0530

    code pushed

---
+++ node/file system/index.js
@@ -0,0 +1,42 @@
+const os = require('node:os')
+
+// console.log(os.totalmem())
+// console.log(os.freemem())
+// console.log(os.version())
+// console.log(os.platform())
+// console.log(os.uptime())
+// console.log(os.userInfo())
+
+const fs = require('node:fs')
+//synchronous
+//asynchronous => non-blocking / background
+
+// let data = fs.readFileSync('sample.txt','utf-8')
+// console.log(data)
+// fs.writeFileSync('sample1.txt','this is new file')
+// fs.appendFileSync('sample1.txt','\nthis is content is added by append')
+// console.log('first')
+// fs.readFile('sample.txt','utf-8',(err,data)=>{
+// if(err) console.log(err)
+//     console.log(data)
+// })
+// console.log('second')
+// fs.writeFile('sample.txt','this file is overrided',(err)=>{
+//    if(err) console.log(err)
+// })
+// fs.appendFile('sample.txt','\nfile is appended ',(err)=>{
+//    if(err) console.log(err)
+// })
+
+// make a progamme to create tables 1-10 in different files and name like 
+//table of 1.txt using loop 
+
+
+
+
+
+
+
+
+
+
