ab42bc222a51176f2805ea6d050b516528c2a3a4
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-27 15:43:09 +0530

    code pushed

---
+++ node/lms backend/models/assignmentModel.js
@@ -0,0 +1,31 @@
+const {Schema , model, default: mongoose} = require('mongoose')
+const assignmentSchema = new Schema({
+    title:{type:String,required:true},
+    description:{type:String,required:true},
+    submissionType:{type:String,required:true},
+    courseId:{type:mongoose.Types.ObjectId,required:true ,
+        Ref:'Course'
+    },
+     status:{type:String,required:true,default:'Active',
+        enum:['Active','Inactive']
+    },
+    points:{
+        type:Number
+    },
+    dueDate:{
+        type:Date
+    }
+},{timestamps:true})
+module.exports = model('Assignment' , assignmentSchema)
+
+// {
+//     id: 1,
+//     title: 'Calculus Problem Set 1',
+//     description: 'Complete problems 1-10 from Chapter 3',
+//     courseId: 1,
+//     dueDate: '2024-02-15',
+//     points: 100,
+//     status: 'Active',
+//     submissionType: 'File Upload',
+//     createdAt: '2024-01-20'
+//   }
